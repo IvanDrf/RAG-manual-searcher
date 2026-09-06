c594283bcb2837fd41de4d80f5b94f1f1425de68
begsta <s.begdash@gmail.com>
2026-09-06 17:44:39 +0300

    feat: convert from pdf to txt

---
+++ books_from_pdf_to_txt.py
@@ -0,0 +1,15 @@
+import os
+import pdfplumber
+from tqdm import tqdm
+
+# Create array of name books from student_books_pdf
+array_name_books = [book for book in os.listdir('./student_book_pdf')]
+
+# Convert pdf to txt
+for book in tqdm(array_name_books):
+    name_book = book.split('.')[0]
+    with pdfplumber.open(f'./student_book_pdf/{name_book}.pdf') as pdf, open(f'./student_book_txt/{name_book}.txt', 'w', encoding='utf-8') as file:
+        for page in pdf.pages:
+            t = page.extract_text()
+            if t:
+                file.write(t + '\n')
