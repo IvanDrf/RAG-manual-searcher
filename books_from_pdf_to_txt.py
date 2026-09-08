2421c683ea0b56a077f846a74a02a84bd30e6414
begsta <s.begdash@gmail.com>
2026-09-08 17:16:21 +0300

    [feat]: delete trash from txt

--- books_from_pdf_to_txt.py
+++ books_from_pdf_to_txt.py
@@ -1,19 +1,41 @@
+from multiprocessing import context
+from pathlib import Path
 import os
-
+import re
 import pdfplumber
 from tqdm import tqdm
 
-# Create array of name books from student_books_pdf
-array_name_books = [book for book in os.listdir("./student_book_pdf")]
+def convert_pdf_to_txt():
+    # Create array of name books from student_books_pdf
+    array_name_books = [book for book in os.listdir("./student_book_pdf")]
 
-# Convert pdf to txt
-for book in tqdm(array_name_books):
-    name_book = book.split(".")[0]
-    with (
-        pdfplumber.open(f"./student_book_pdf/{name_book}.pdf") as pdf,
-        open(f"./student_book_txt/{name_book}.txt", "w", encoding="utf-8") as file,
-    ):
-        for page in pdf.pages:
-            t = page.extract_text()
-            if t:
-                file.write(t + "\n")
+    # Convert pdf to txt
+    for book in tqdm(array_name_books):
+        name_book = book.split(".")[0]
+        with (
+            pdfplumber.open(f"./student_book_pdf/{name_book}.pdf") as pdf,
+            open(f"./student_book_txt/{name_book}.txt", "w", encoding="utf-8") as file,
+        ):
+            for page in pdf.pages:
+                t = page.extract_text()
+                if t:
+                    file.write(t + "\n")
+
+def fixing_bad_words_in_txt():
+
+    array_txt_books = [book for book in os.listdir('./student_book_txt')]
+
+    for book in tqdm(array_txt_books):
+        with open(f"./student_book_txt/{book}", mode='r+', encoding='utf-8') as file:
+            content = file.read()
+
+            cleaned_content = re.sub(r"^.*[À-ÿ].*$\n?", "", content, flags=re.MULTILINE)
+
+            file.seek(0)
+
+            file.write(cleaned_content)
+
+            file.truncate()
+
+if __name__ == "__main__":
+    fixing_bad_words_in_txt()
