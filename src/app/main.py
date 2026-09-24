6e626b107a8302750dfa1c5f2f1f5e44e1d9cead
IvanDrf <ivandrgmr@gmail.com>
2026-09-24 21:54:44 +0300

    [dev]: add metrics port

--- src/app/main.py
+++ src/app/main.py
@@ -3,6 +3,7 @@ from time import perf_counter
 
 from fastapi import FastAPI, Request, status
 from loguru import logger
+from prometheus_fastapi_instrumentator.instrumentation import PrometheusFastApiInstrumentator
 from uvicorn import run
 
 from src.api.admin import admin_router
@@ -25,6 +26,8 @@ app = FastAPI(title="RAG-research app", lifespan=lifespan)
 app.include_router(auth_router)
 app.include_router(admin_router)
 app.include_router(chat_router)
+
+PrometheusFastApiInstrumentator().instrument(app).expose(app)
 
 
 @app.get("/health", status_code=status.HTTP_200_OK, description="health check для проверки сервера на работоспособность")
